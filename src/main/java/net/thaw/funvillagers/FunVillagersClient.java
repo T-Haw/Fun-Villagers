11c93a4537b3ed16be723bf9efe0d4bacb870857
Adomas <asileiks@gmail.com>
2023-07-26 15:02:45 +0300

    yeah

---
+++ src/main/java/net/thaw/funvillagers/FunVillagersClient.java
@@ -0,0 +1,10 @@
+package net.thaw.funvillagers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FunVillagersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/thaw/funvillagers/FunVillagersDataGenerator.java
@@ -0,0 +1,11 @@
+package net.thaw.funvillagers;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FunVillagersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
